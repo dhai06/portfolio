91001356067708d1706ad42654989d095f06618b
dhai06 <dhai06@student.ubc.ca>
2026-01-15 21:38:24 -0800

    fixed spotify api issue

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,13 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '/image/**',
+      },
+    ],
   },
 };
 
